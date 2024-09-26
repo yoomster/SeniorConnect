a85248b57dbc1ad502a812c069c58b3b537736c9
NaomiPerenboom <NaomiPerenboom@DESKTOP-MQIL4FD>
2024-09-26 09:29:39 +0200

    Add project files.

---
+++ SeniorConnect.WebApp/Pages/IdentityAccessManagement/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeniorConnect.WebApp.Pages.IdentityAccessManagement
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ SeniorConnect.WebApp/Pages/IdentityAccessManagement/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SeniorConnect.WebApp.Pages.IdentityAccessManagement
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
