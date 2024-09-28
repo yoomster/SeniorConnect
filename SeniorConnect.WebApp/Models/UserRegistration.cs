090b9e72b940a77d0a8052e730978a4b1ce77d26
NaomiP <nperenboom@student>
2024-09-28 13:43:55 +0200

    Added user registration class

---
+++ SeniorConnect.WebApp/Models/UserRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SeniorConnect.WebApp.Models
+{
+    public class UserRegistration
+    {
+        [Required(ErrorMessage = "Required field")]
+        public string FirstName { get; init; }
+
+        [Required(ErrorMessage = "Required field")]
+        public string LastName { get; init; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; init; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DataType(DataType.Password)]
+        public string Password { get; init; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; init; }
+    }
+}
