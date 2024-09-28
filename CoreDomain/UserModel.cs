340c411b42f8501d42bc73a1593ab66dee09e9d7
NaomiP <nperenboom@student>
2024-09-28 12:50:10 +0200

    Added rewuirements to user model

--- CoreDomain/UserModel.cs
+++ CoreDomain/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,23 @@ namespace CoreDomain
 {
     public class UserModel
     {
-        public int Id { get; set; }
+        [Required(ErrorMessage = "Required field")]
+        public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Required field")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
     }
 }
