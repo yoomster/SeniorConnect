e97819a0000c6a8f654e4011244aaf9f014674f7
NaomiP <nperenboom@student>
2024-09-28 13:45:15 +0200

    Added register to the menu bar and databinding from register page to registrationclass

--- SeniorConnect.WebApp/Pages/IdentityAccessManagement/Register.cshtml.cs
+++ SeniorConnect.WebApp/Pages/IdentityAccessManagement/Register.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SeniorConnect.WebApp.Models;
 
 namespace SeniorConnect.WebApp.Pages.IdentityAccessManagement
 {
     public class RegisterModel : PageModel
     {
+        [BindProperty]
+        public UserRegistration Registration { get; set; }
+        
         public void OnGet()
         {
+        }
+
+        public ActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("Login");
         }
     }
 }
