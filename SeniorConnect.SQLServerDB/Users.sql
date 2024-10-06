193026463b4f57617d7454b100220e59b903af57
NaomiP <nperenboom@hotmail.com>
2024-10-06 22:03:57 +0200

    Added SQL Server DB project with Users table

---
+++ SeniorConnect.SQLServerDB/Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(60) NOT NULL,
+    [Email] NVARCHAR(70) NOT NULL,
+    [Password] NVARCHAR(30) NOT NULL, 
+
+)
