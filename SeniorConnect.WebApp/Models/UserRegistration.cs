5808e09b32abd341f5a5479f05290b31131c8dfa
NaomiP <nperenboom@hotmail.com>
2024-10-14 14:10:05 +0200

    fix to navigation between login and registration

--- SeniorConnect.WebApp/Models/UserRegistration.cs
+++ SeniorConnect.WebApp/Models/UserRegistration.cs
@@ -4,21 +4,21 @@ namespace SeniorConnect.WebApp.Models
 {
     public class UserRegistration
     {
-        [Required(ErrorMessage = "Required field")]
+        [Required(ErrorMessage = "Required field!")]
         public string FirstName { get; init; }
 
-        [Required(ErrorMessage = "Required field")]
+        [Required(ErrorMessage = "Required field!")]
         public string LastName { get; init; }
 
-        [Required(ErrorMessage = "Required field")]
+        [Required(ErrorMessage = "Required field!")]
         [DataType(DataType.EmailAddress)]
         public string EmailAddress { get; init; }
 
-        [Required(ErrorMessage = "Required field")]
+        [Required(ErrorMessage = "Required field!")]
         [DataType(DataType.Password)]
         public string Password { get; init; }
 
-        [Required(ErrorMessage = "Required field")]
+        [Required(ErrorMessage = "Required field!")]
         [DataType(DataType.Password)]
         [Compare("Password")]
         public string ConfirmPassword { get; init; }
