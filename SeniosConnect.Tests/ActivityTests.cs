315efa573efe7f65c7a6a1e9ad224fc7611a13a6
yoomster <nperenboom@hotmail.com>
2024-11-03 20:22:45 +0100

    Reserve activity test fixed and working

--- SeniosConnect.Tests/ActivityTests.cs
+++ SeniosConnect.Tests/ActivityTests.cs
@@ -6,31 +6,22 @@ namespace SeniorConnect.Tests
 {
     public class ActivityTests
     {
-        private readonly Activity _sut = new(1, Guid.NewGuid(), Guid.NewGuid());
+       
         [Fact]
         public void ReserveSpot_ShouldFailReservation_WhenNoMoreRoom()
         {
             //Arrange
-            //Create 2 participants
-            var participant1 = new Participant();
-            var participant2 = new Participant();
+            var activity = new Activity(1, Guid.NewGuid(), Guid.NewGuid());
+
+            var participant1= new Participant (Guid.NewGuid());
+            var participant2= new Participant(Guid.NewGuid());
 
             //Act
-            //add both participants 
-            Action result = () => _sut.ReserveSpot(participant1);
-            Action result2 = () => _sut.ReserveSpot(participant2);
-            //activity.ReserveSpot(participant2);
+            activity.ReserveSpot(participant1);
+            var action = () => activity.ReserveSpot(participant2);
 
             //Assert
-            //participant 2 reservation should fail
-            result.Should().Throw<NotImplementedException>();
-            result2.Should().Throw<NotImplementedException>();
-            
-
-
+            action.Should().ThrowExactly<InvalidOperationException>();
         }
-
-
-
     }
 }
