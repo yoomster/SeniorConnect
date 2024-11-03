0382fae8562e94c54f0086ef8ee2c6e81342b08a
yoomster <nperenboom@hotmail.com>
2024-11-03 20:22:03 +0100

    fix to reservespot method

---
+++ CoreDomain/Activity/Activity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeniorConnect.Domain.Users;
+
+namespace SeniorConnect.Domain.Activity
+{
+    public class Activity
+    {
+        private readonly Guid _id;
+        private readonly int _maxParticipants;
+        private readonly Guid _activityCoordinatorId;
+        private readonly List<Participant> _participants = new();
+        private readonly Guid _locationId;
+
+
+        public Activity(
+            int maxParticipants,
+            Guid activityCoordinatorId,
+            Guid locationId,
+            Guid? id = null )
+        {
+            _id = id ?? Guid.NewGuid();
+            _maxParticipants = maxParticipants;
+            _activityCoordinatorId = activityCoordinatorId;
+            _locationId = locationId;
+        }
+
+        //public Activity(int maxParticipants)
+        //{
+        //    _maxParticipants = maxParticipants;
+        //}
+
+
+
+        public void ReserveSpot(Participant participant)
+        {
+            //bool isFullyBooked;
+
+            if (_participants.Count() >= _maxParticipants)
+            {
+                //The system displays a message indicating that the activity is full. 
+                throw new InvalidOperationException ("maximum participants reached");
+            }
+            else
+            {
+                _participants.Add(participant);
+                //Add the activity ID to participant 
+                participant.AddActivityToList(_id);
+            }
+        }
+    }
+}
--- CoreDomain/Activity/ActivityCoordinator.cs
+++ CoreDomain/Activity/ActivityCoordinator.cs
@@ -4,13 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CoreDomain
+namespace SeniorConnect.Domain.Activity
 {
     public class ActivityCoordinator
     {
         private readonly Guid _id;
         private readonly Guid _userId;
-        private readonly List <Guid> _activityIds;
+        private readonly List<Guid> _activityIds = new();
 
     }
 }
