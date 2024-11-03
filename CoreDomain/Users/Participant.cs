2f1747027c8246007e96546d8f7df5a4ec36499e
yoomster <nperenboom@hotmail.com>
2024-11-03 19:29:50 +0100

    added folder

--- CoreDomain/Users/Participant.cs
+++ CoreDomain/Users/Participant.cs
@@ -4,14 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CoreDomain
+namespace SeniorConnect.Domain.Users
 {
     public class Participant
     {
         private readonly Guid _id;
         private readonly Guid _userId;
         private readonly Guid _subscriptionId;
-        private readonly List<Guid> _activityIds;
+        private readonly List<Guid> _activityIds = new();
+
+        public Participant(Guid? id = null)
+        {
+            _id = id ?? Guid.NewGuid();
+        }
 
     }
 }
