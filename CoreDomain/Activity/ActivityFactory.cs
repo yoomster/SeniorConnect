fcc917ea1bc398798f070957c08eead992bcdeb3
yoomster <nperenboom@hotmail.com>
2024-11-08 00:05:36 +0100

    trying out to use a factory for creating new activity

---
+++ CoreDomain/Activity/ActivityFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace SeniorConnect.Domain.Activity
+{
+    public static class ActivityFactory
+    {
+        public static Activity CreateActivity(
+            DateOnly? date = null,
+            TimeOnly? startTime = null,
+            TimeOnly? endTime = null,
+            int maxParticipants = Activity.MaxParticipants,
+            Guid? activityCoordinatorId = null,
+            Guid? locationId = null,
+            Guid? id = null)
+        {
+            return new Activity {
+            date ?? Activity.Date,
+            startTime ?? Activity.StartTime,
+            endTime ?? Activity.Activity.EndTime,
+            maxParticipants,
+            ActivityCoordinatiorId
+            id: id ?? Activity.Id;
+            };
+        }
+    }
+}
--- SeniosConnect.Tests/ActivityTests.cs
+++ SeniosConnect.Tests/ActivityTests.cs
@@ -11,7 +11,8 @@ namespace SeniorConnect.Tests
         public void ReserveSpot_ShouldFailReservation_WhenNoMoreRoom()
         {
             //Arrange
-            var activity = new Activity(1, Guid.NewGuid(), Guid.NewGuid());
+            var activity = ActivityFactory.CreateActivity();
+                //new Activity(1, Guid.NewGuid(), Guid.NewGuid());
 
             var participant1 = new Participant (Guid.NewGuid());
             var participant2 = new Participant(Guid.NewGuid());
