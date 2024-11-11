a5fd2d0814fb1a5d73acf05ad89e7eb7932333cf
NaomiP <nperenboom@hotmail.com>
2024-11-11 22:45:32 +0100

    fixed the activity model

--- CoreDomain/Activity/Activity.cs
+++ CoreDomain/Activity/Activity.cs
@@ -1,66 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SeniorConnect.Domain.Users;
+﻿using SeniorConnect.Domain.Users;
 
-namespace SeniorConnect.Domain.Activity
+namespace SeniorConnect.Domain.Activity;
+
+public class Activity
 {
-    public class Activity
+    private readonly string _name;
+    private readonly string _description;
+    private readonly DateOnly _date;
+    private readonly TimeOnly _startTime;
+    private readonly TimeOnly _endTime;
+    private readonly Guid _id;
+    private readonly int _maxParticipants;
+    private readonly ActivityCoordinatorId _activityCoordinatorId;
+    private readonly List<Participant> _participants = new();
+    private readonly LocationId _locationId;
+
+
+    public Activity(
+        DateOnly dateOfActivity,
+        TimeOnly startTime,
+        TimeOnly endTime,
+        int maxParticipants,
+        ActivityCoordinatorId activityCoordinatorId,
+        LocationId locationId
+        )
     {
-        private readonly DateOnly _date;
-        private readonly TimeOnly _startTime;
-        private readonly TimeOnly _endTime;
-        private readonly Guid _id;
-        private readonly int _maxParticipants;
-        private readonly Guid _activityCoordinatorId;
-        private readonly List<Participant> _participants = new();
-        private readonly Guid _locationId;
+        _date = dateOfActivity;
+        _startTime = startTime;
+        _endTime = endTime;
+        _maxParticipants = maxParticipants;
+        _activityCoordinatorId = activityCoordinatorId;
+        _locationId = locationId;
+    }
 
+    public void CancelReservation(Participant participant)
+    {
+        //check if allowed
+        //session time - current time <24h
+      
+    }
 
-        public Activity(
-            DateOnly date,
-            TimeOnly startTime,
-            TimeOnly endTime,
-            int maxParticipants,
-            Guid activityCoordinatorId,
-            Guid locationId,
-            Guid? id = null )
+    public void ReserveSpot(Participant participant)
+    {
+        //bool isFullyBooked;
+
+        if (_participants.Count() >= _maxParticipants)
         {
-            _date = date;
-            _startTime = startTime;
-            _endTime = endTime;
-            _id = id ?? Guid.NewGuid();
-            _maxParticipants = maxParticipants;
-            _activityCoordinatorId = activityCoordinatorId;
-            _locationId = locationId;
+            //todo: he system displays a message indicating that the activity is full. 
+            throw new InvalidOperationException ("maximum participants reached");
         }
-
-        public void CancelReservation(Participant participant)
+        else
         {
-            //check if allowed
-            //session time - current time <24h
-        }
-
-
-
-        public void ReserveSpot(Participant participant)
-        {
-            //bool isFullyBooked;
-
-            if (_participants.Count() >= _maxParticipants)
-            {
-                //todo: he system displays a message indicating that the activity is full. 
-                throw new InvalidOperationException ("maximum participants reached");
-            }
-            else
-            {
-                _participants.Add(participant);
-                //Add the activity ID to participant 
-                participant.AddActivityToList(_id);
-            }
+            _participants.Add(participant);
+            //Add the activity ID to participant 
+            participant.AddActivityToList(_id);
         }
     }
 }
