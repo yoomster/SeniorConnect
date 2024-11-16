8789173ba64a6caeaf3bb5a362f29a52fc1287ee
NaomiP <nperenboom@hotmail.com>
2024-11-16 18:30:23 +0100

    Added data access library

---
+++ SeniorConnect.DataAccesLibrary/DataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeniorConnect.DataAccesLibrary
+{
+    public class DataAccess
+    {
+        public void LoadData()
+        {
+
+        }
+
+        public void SaveData()
+        {
+
+        }
+    }
+}
