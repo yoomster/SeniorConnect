9153ece760f5607e83e30204bedc4514c85c285f
NaomiP <nperenboom@hotmail.com>
2024-11-16 16:05:44 +0100

    changes to the activity and users tables

---
+++ SeniorConnect.SQLServerDB/dbo/Tables/Activity.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Activity]
+(
+	[ActivityId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NOT NULL, 
+    [Description] NVARCHAR(500) NULL, 
+    [Date] DATE NOT NULL, 
+    [StartTime] TIME NOT NULL,
+    [EndTime] TIME NOT NULL,
+    [MaxParticipants] INT NOT NULL, 
+)
---
+++ SeniorConnect.SQLServerDB/dbo/Tables/Users.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[UserId] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(100) NOT NULL, 
+    [LastName] NVARCHAR(100) NOT NULL,
+    [Email] NVARCHAR(100) NOT NULL,
+    [Password] NVARCHAR(50) NOT NULL, 
+    [DateOfBirth] DATETIMEOFFSET NOT NULL, 
+    [Zipcode] NVARCHAR(10) NOT NULL, 
+    [City] NVARCHAR(50) NOT NULL, 
+    [Country] NVARCHAR(50) NOT NULL, 
+    [Gender] CHAR NULL, 
+    [Origin] NVARCHAR(50) NULL, 
+    [IBAN] CHAR(20) NOT NULL, 
+    [DateOfRegistration] DATE NOT NULL, 
+)
