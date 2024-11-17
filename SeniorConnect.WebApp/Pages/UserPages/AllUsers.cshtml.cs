c978bf3d348cf254345aaf98fc2219b95a38783b
NaomiP <nperenboom@hotmail.com>
2024-11-17 21:40:20 +0100

    Added page to list all users in the DB

---
+++ SeniorConnect.WebApp/Pages/UserPages/AllUsers.cshtml.cs
@@ -0,0 +1,24 @@
+using CoreDomain.Users;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SeniorConnect.DataAccesLibrary;
+
+namespace SeniorConnect.WebApp.Pages.UserPages
+{
+    public class AllUsersModel : PageModel
+    {
+        private readonly DataAccess _dataAccess;
+
+        public List<User> Users { get; set; } = new List<User>();
+
+        public AllUsersModel(DataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public void OnGet()
+        {
+            Users = _dataAccess.GetAllUsers();
+        }
+    }
+}
