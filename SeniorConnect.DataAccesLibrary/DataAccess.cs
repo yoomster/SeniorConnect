429a52242a64b3a9bdeb610eceb0ab163c203808
NaomiP <nperenboom@hotmail.com>
2024-11-23 16:37:42 +0100

    implemented security by data access and use of a user repository

--- SeniorConnect.DataAccesLibrary/DataAccess.cs
+++ SeniorConnect.DataAccesLibrary/DataAccess.cs
@@ -14,6 +14,63 @@ namespace SeniorConnect.DataAccesLibrary
             return connection;
         }
 
-        
+        protected List<User> GetAllUsers()
+        {
+            var users = new List<User>();
+
+            string query = "SELECT [Id], [FirstName], [LastName], [Email], [Password] FROM [dbo].[User]";
+
+            try
+            {
+                using (var connection = OpenSqlConnection(_connectionString))
+                using (var command = new SqlCommand(query, connection))
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var user = new User
+                        {
+                            Id = reader.GetInt32(0),
+                            FirstName = reader.GetString(1),
+                            LastName = reader.GetString(2),
+                            Email = reader.GetString(3),
+                            Password = reader.GetString(4)
+                        };
+
+                        users.Add(user);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error loading data: " + ex.Message);
+            }
+
+            return users;
+        }
+
+        protected void SaveUser(User user)
+        {
+            string query = "INSERT INTO [SeniorConnect].[dbo].[User] ([FirstName], [LastName], [Email], [Password])";
+
+            try
+            {
+                using (var connection = OpenSqlConnection(_connectionString))
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", user.FirstName);
+                    command.Parameters.AddWithValue("@LastName", user.LastName);
+                    command.Parameters.AddWithValue("@Email", user.Email);
+                    command.Parameters.AddWithValue("@Password", user.Password);
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error saving data: " + ex.Message);
+
+            }
+        }
     }
 }
---
+++ SeniorConnect.DataAccesLibrary/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using CoreDomain.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeniorConnect.DataAccesLibrary
+{
+    public class UserRepository : DataAccess
+    {
+        public List<User> GetAllUsersFromDB()
+        {
+            return base.GetAllUsers(); 
+        }
+
+        public void SaveUserToDB(User user)
+        {
+            base.SaveUser(user); 
+        }
+    }
+}
