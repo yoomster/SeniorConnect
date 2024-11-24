48c376207905e73e3f798f0e5d53dfbe149c0bbe
NaomiP <nperenboom@hotmail.com>
2024-11-24 17:11:47 +0100

    Regitered services for data access & user repo

--- SeniorConnect.WebApp/Program.cs
+++ SeniorConnect.WebApp/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<DataAccess>();
+builder.Services.AddScoped<UserRepository>();
+
 
 var app = builder.Build();
 
