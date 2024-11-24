6b966aba42da8d5ffc6885be8183c052e3cad316
NaomiP <nperenboom@hotmail.com>
2024-11-24 17:14:19 +0100

    Added user repo to handle adding new user + reading db with query rather then through data access

--- SeniorConnect.DataAccesLibrary/DataAccess.cs
+++ SeniorConnect.DataAccesLibrary/DataAccess.cs
@@ -5,7 +5,9 @@ namespace SeniorConnect.DataAccesLibrary
 {
     public class DataAccess
     {
+        //private readonly string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SeniorConnect.SQLServerDB;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Connect Timeout=60;Encrypt=True;Trust Server Certificate=False;Command Timeout=0";
         private readonly string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SeniorConnect;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
 
         private static SqlConnection OpenSqlConnection(string connectionString)
         {
@@ -14,63 +16,9 @@ namespace SeniorConnect.DataAccesLibrary
             return connection;
         }
 
-        protected List<User> GetAllUsers()
+        public SqlConnection OpenSqlConnection()
         {
-            var users = new List<User>();
-
-            string query = "SELECT [Id], [FirstName], [LastName], [Email], [Password] FROM [dbo].[User]";
-
-            try
-            {
-                using (var connection = OpenSqlConnection(_connectionString))
-                using (var command = new SqlCommand(query, connection))
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var user = new User
-                        {
-                            Id = reader.GetInt32(0),
-                            FirstName = reader.GetString(1),
-                            LastName = reader.GetString(2),
-                            Email = reader.GetString(3),
-                            Password = reader.GetString(4)
-                        };
-
-                        users.Add(user);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Error loading data: " + ex.Message);
-            }
-
-            return users;
-        }
-
-        protected void SaveUser(User user)
-        {
-            string query = "INSERT INTO [SeniorConnect].[dbo].[User] ([FirstName], [LastName], [Email], [Password])";
-
-            try
-            {
-                using (var connection = OpenSqlConnection(_connectionString))
-                using (var command = new SqlCommand(query, connection))
-                {
-                    command.Parameters.AddWithValue("@FirstName", user.FirstName);
-                    command.Parameters.AddWithValue("@LastName", user.LastName);
-                    command.Parameters.AddWithValue("@Email", user.Email);
-                    command.Parameters.AddWithValue("@Password", user.Password);
-                    command.ExecuteNonQuery();
-                }
-            }
-            catch (Exception ex)
-            {
-
-                Console.WriteLine("Error saving data: " + ex.Message);
-
-            }
+            return OpenSqlConnection(_connectionString);
         }
     }
 }
--- SeniorConnect.DataAccesLibrary/UserRepository.cs
+++ SeniorConnect.DataAccesLibrary/UserRepository.cs
@@ -1,22 +1,78 @@
 ﻿using CoreDomain.Users;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace SeniorConnect.DataAccesLibrary
 {
-    public class UserRepository : DataAccess
+    public class UserRepository
     {
-        public List<User> GetAllUsersFromDB()
+        private readonly DataAccess _dataAccess;
+
+        public UserRepository(DataAccess dataAccess)
         {
-            return base.GetAllUsers(); 
+            _dataAccess = dataAccess;
         }
 
         public void SaveUserToDB(User user)
         {
-            base.SaveUser(user); 
+            string query = "INSERT INTO [SeniorConnect].[dbo].[User] ([FirstName], [LastName], [Email], [Password]) " +
+                           "VALUES (@FirstName, @LastName, @Email, @Password)";
+
+            try
+            {
+                using (var connection = _dataAccess.OpenSqlConnection())
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@FirstName", user.FirstName);
+                    command.Parameters.AddWithValue("@LastName", user.LastName);
+                    command.Parameters.AddWithValue("@Email", user.Email);
+                    command.Parameters.AddWithValue("@Password", user.Password);  // password hash needed?
+
+                    command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                // change to handle error!!!
+                Console.WriteLine("Error saving data: " + ex.Message);
+            }
+        }
+
+
+        public List<User> GetUsersV2()
+        {
+            var users = new List<User>();
+
+            string query = "SELECT [Id], [FirstName], [LastName], [Email], [Password] FROM [dbo].[User]";
+
+            try
+            {
+                using (var connection = _dataAccess.OpenSqlConnection())
+                using (var command = new SqlCommand(query, connection))
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var user = new User
+                        {
+                            Id = reader.GetInt32(0),
+                            FirstName = reader.GetString(1),
+                            LastName = reader.GetString(2),
+                            Email = reader.GetString(3),
+                            Password = reader.GetString(4)
+                        };
+
+                        users.Add(user);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // change to handle error!!!
+                Console.WriteLine("Error loading data: " + ex.Message);
+            }
+
+            return users;
+
         }
     }
 }
