75b7b51393138167ee191be0aefc71aacc3032c9
NaomiP <nperenboom@hotmail.com>
2024-12-06 17:11:47 +0100

    New script for adding dummy activity data

---
+++ SeniorConnect.SQLServerDB/Scripts/InsertDummyActivities.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].Activity ([Name], [Description], [Date], [StartTime], [EndTime], [MaxParticipants])
+VALUES 
+('Dames Bingo', 'Lekker bingoen met de meiden', 01/03/2025, 19.00, 20.00, 20),
+('Stoelen dans', 'Lekker swingen terwijl je een stoel probeert te bemachtigen', 01/03/2025, 20.00, 20.00, 15)
