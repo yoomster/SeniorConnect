c9e60c5c8f45eb3e554c6ea53b4a1c15aac0a5dc
NaomiP <nperenboom@hotmail.com>
2024-12-15 16:42:01 +0100

    latest

--- SeniorConnect.DataAccesLibrary/DataAccess.cs
+++ SeniorConnect.DataAccesLibrary/DataAccess.cs
@@ -5,8 +5,8 @@ namespace SeniorConnect.DataAccesLibrary
 {
     public class DataAccess
     {
-        private readonly string _connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SeniorConnect.SQLServerDB;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Connect Timeout=60;Encrypt=True;Trust Server Certificate=False;Command Timeout=0";
-        private static SqlConnection OpenSqlConnection(string connectionString)
+        private readonly string _connectionString = "";
+            private static SqlConnection OpenSqlConnection(string connectionString)
         {
             var connection = new SqlConnection(connectionString);
             connection.Open();
