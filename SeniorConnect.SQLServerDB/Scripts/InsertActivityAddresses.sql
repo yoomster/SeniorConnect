ef440df37f6ff3912b3c7f4de62af22ff22ae9b9
yoomster <nperenboom@hotmail.com>
2024-12-15 19:35:53 +0100

    updated scripts

---
+++ SeniorConnect.SQLServerDB/Scripts/InsertActivityAddresses.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO [dbo].[Address] 
+(StreetName, HouseNumber, Zipcode, City, Country)
+VALUES
+('Lindelaan', '12', '1234AB', 'Amsterdam', 'Netherlands'),
+('Kerkstraat', '24', '5678CD', 'Rotterdam', 'Netherlands'),
+('Zonnelaan', '5', '2345EF', 'Utrecht', 'Netherlands'),
+('Dorpstraat', '45', '3456GH', 'Eindhoven', 'Netherlands'),
+('Wilhelminaplein', '78', '4567IJ', 'Groningen', 'Netherlands'),
+('Beatrixstraat', '6', '5678KL', 'Maastricht', 'Netherlands');
--- SeniorConnect.SQLServerDB/Scripts/InsertDummyActivities.sql
+++ SeniorConnect.SQLServerDB/Scripts/InsertDummyActivities.sql
@@ -1,18 +1,9 @@
-﻿INSERT INTO [dbo].[Activity] 
-(Name, Description, Date, StartTime, EndTime, MaxParticipants)
+﻿INSERT INTO [dbo].[Activity]
+(Name, Description, Date, StartTime, EndTime, MaxParticipants, AddressId)
 VALUES
-('Classic Movie Screening', 
- 'Enjoy a screening of a classic movie with light refreshments and a nostalgic atmosphere.', 
- '2024-12-15', '14:00:00', '16:30:00', 30),
-('Historical Walking Tour', 
- 'A leisurely guided tour of the city\'s historical landmarks, complete with stories and anecdotes.', 
- '2024-12-20', '10:00:00', '12:30:00', 30),
-('Gardening Workshop', 
- 'Learn tips and tricks for creating and maintaining a beautiful garden from an expert.', 
- '2025-01-05', '09:00:00', '12:00:00', 20),
-('Afternoon Tea Social', 
- 'A relaxing afternoon with tea, coffee, and pastries, combined with light music and conversation.', 
- '2025-01-10', '14:00:00', '16:00:00', 40),
-('Health & Wellness Seminar', 
- 'An informative session on maintaining health and wellness with practical tips for seniors.', 
- '2025-01-20', '10:00:00', '12:00:00', 100);
+('Gentle Yoga for Seniors', 'A calm and gentle yoga class designed for seniors to improve flexibility and balance.', '2024-12-15', '09:00', '10:00', 10, 21),
+('Chair Aerobics', 'Low-impact aerobics using a chair for support, perfect for seniors to stay active.', '2024-12-17', '10:00', '11:00', 15, 22),
+('Senior Walking Group', 'A walking group for seniors to enjoy a leisurely walk and socialize in a park.', '2024-12-18', '14:00', '15:30', 20, 23),
+('Cooking for Health', 'A fun and educational cooking class for seniors, focusing on healthy, easy-to-make meals.', '2024-12-19', '11:00', '12:30', 12, 24),
+('Art Therapy for Seniors', 'An art class focused on creative expression and relaxation for seniors.', '2024-12-20', '13:00', '14:30', 10, 25),
+('Music Therapy for Seniors', 'A therapeutic music session designed to bring joy and emotional benefits to seniors.', '2024-12-21', '15:00', '16:30', 12, 26);
