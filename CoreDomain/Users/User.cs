b216230905a9d27bcf23fcf1b96e4fe5e8e63f75
yoomster <nperenboom@hotmail.com>
2024-12-15 22:51:47 +0100

    seperation of address model, inline with db

--- CoreDomain/Users/User.cs
+++ CoreDomain/Users/User.cs
@@ -18,10 +18,6 @@ public class User
     public char? Gender { get; set; } = 'N';
     public string? Iban { get; set; }
     public DateOnly DateOfRegistration { get; set; } = DateOnly.FromDateTime(DateTime.Now);
-    public string ? StreetName { get; set; }
-    public string? HouseNumber { get; set; }
-    public string? Zipcode { get; set; }
-    public string? City { get; set; }
-    public string? Country { get; set; } 
+
 
 }
