2b42023a0f3621157440c42a0674f79cda44f47c
yoomster <nperenboom@hotmail.com>
2024-12-15 17:58:07 +0100

    no more hard coded connection string, instead injected from appsettings

--- SeniorConnect.DataAccesLibrary/DataAccess.cs
+++ SeniorConnect.DataAccesLibrary/DataAccess.cs
@@ -1,12 +1,19 @@
 ﻿using CoreDomain.Users;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 
 namespace SeniorConnect.DataAccesLibrary
 {
     public class DataAccess
     {
-        private readonly string _connectionString = "";
-            private static SqlConnection OpenSqlConnection(string connectionString)
+        private readonly string _connectionString;
+
+        public DataAccess(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("Default");
+        }
+
+        private static SqlConnection OpenSqlConnection(string connectionString)
         {
             var connection = new SqlConnection(connectionString);
             connection.Open();
