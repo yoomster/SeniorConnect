d8c0db45a4293769611767edb7038a56f030977e
yoomster <nperenboom@hotmail.com>
2024-12-15 17:56:52 +0100

    cleanup of the db tables, now matching the ERD

--- SeniorConnect.SQLServerDB/dbo/Tables/User.sql
+++ SeniorConnect.SQLServerDB/dbo/Tables/User.sql
@@ -1,8 +1,19 @@
 ﻿CREATE TABLE [dbo].[User]
 (
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+	[UserId] INT NOT NULL PRIMARY KEY IDENTITY,
     [FirstName] NVARCHAR(50) NOT NULL, 
     [LastName] NVARCHAR(50) NOT NULL,
     [Email] NVARCHAR(70) NOT NULL,
-    [Password] NVARCHAR(50) NOT NULL
+    [Password] NVARCHAR(50) NOT NULL,
+    [DateOfBirth] Date NOT NULL,
+    --1 for TRUE and 0 for FALSE
+    [Gender] char, 
+    [Origin] nvarchar(30),
+    [Iban] nvarchar(30),
+    [DateOfRegistration] Date NOT NULL,
+    [StreetName] NVARCHAR(50) NOT NULL, 
+	[HouseNumber] NVARCHAR(10) NOT NULL, 
+	[Zipcode] NVARCHAR(10) NOT NULL, 
+    [City] NVARCHAR(50) NOT NULL, 
+    [Country] NVARCHAR(30) NOT NULL,
 );
--- SeniorConnect.SQLServerDB/dbo/Tables/UserAddress.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[UserAddress]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-	[UserId] INT NOT NULL, 
-	[AddressId] INT NOT NULL
-)
