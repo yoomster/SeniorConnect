481436f10cd88d53b7a5e1fe7f31898212d666a5
yoomster <nperenboom@hotmail.com>
2024-12-19 17:24:03 +0100

    Using services for doing validation on domain classes

---
+++ CoreDomain/Services/ActivityService.cs
@@ -0,0 +1,28 @@
+﻿using SeniorConnect.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeniorConnect.Domain.Services
+{
+    public class ActivityService
+    {
+        private readonly IActivityRepository _activityRepository;
+
+        public ActivityService(IActivityRepository activityRepository)
+        {
+            _activityRepository = activityRepository;
+        }
+
+        public static void RegisterActivity (object registerDto, object userDto)
+        {
+            throw new NotImplementedException();
+            //if statement for validation, if true, create
+        }
+
+        //An activity cannot contain more than the maximum number of participants
+        //A reservation cannot be canceled for free less than 24 hours before the session starts
+    }
+}
---
+++ CoreDomain/Services/UserService.cs
@@ -0,0 +1,34 @@
+﻿using SeniorConnect.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeniorConnect.Domain.Services
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public static void RegisterUser(object registerDto, object userDto)
+        {
+            throw new NotImplementedException();
+            //if statement for validation, if true, create
+        }
+
+        //Minimumleeftijd?
+
+        //hashing van het wachtwoord?
+        private string HashPassword(string password)
+        {
+            //impl. hashing logic
+            return password;
+        }
+    }
+}
