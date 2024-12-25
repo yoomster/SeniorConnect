24abf5a2c07c64bb53099cf0ecbac13afbef076c
yoomster <nperenboom@hotmail.com>
2024-12-25 15:12:56 +0100

    page now sending to user service for validation and persistance

--- SeniorConnect.WebApp/Pages/IdentityAccessManagement/Register.cshtml.cs
+++ SeniorConnect.WebApp/Pages/IdentityAccessManagement/Register.cshtml.cs
@@ -1,28 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SeniorConnect.WebApp.Models;
-using SeniorConnect.DataAccesLibrary;
-using CoreDomain.Users;
-using SeniorConnect.Domain.Contracts;
-using System.Reflection;
-using CoreDomain;
 using SeniorConnect.Domain.Services;
+using SeniorConnect.WebApp.Mapping;
 
 namespace SeniorConnect.WebApp.Pages.IdentityAccessManagement
 {
     public class RegisterModel : PageModel
     {
-        private readonly IUserRepository _userRepository;
-        private readonly IAddressRepository _addressRepository;
 
         [BindProperty]
-        public UserUI User { get; set; }
-
-        public RegisterModel(IUserRepository userRepository, IAddressRepository addressRepository)
-        {
-            _userRepository = userRepository;
-            _addressRepository = addressRepository;
-        }
+        public UserFormModel UserFormModel { get; set; }
 
         public void OnGet()
         {
@@ -37,37 +25,8 @@ namespace SeniorConnect.WebApp.Pages.IdentityAccessManagement
 
             try
             {
-                //verander dit naar een CreateAddress method uit domein!
-                var newAddress = new Address
-                {
-                    StreetName = User.StreetName,
-                    HouseNumber = User.HouseNumber,
-                    Zipcode = User.Zipcode,
-                    City = User.City,
-                    Country = User.Country,
-                };
-
-                var addressId = await _addressRepository.SaveAddressToDBAsync(newAddress);
-
-                //verander dit naar een CreateUser method uit domein!
-                var newUser = new User
-                {
-                    FirstName = User.FirstName,
-                    LastName = User.LastName,
-                    Email = User.Email,
-                    Password = User.Password,
-                    DateOfBirth = DateOnly.FromDateTime(User.DateOfBirth),
-                    Gender = User.Gender,
-                    Origin = User.Origin,
-                    DateOfRegistration = DateOnly.FromDateTime(DateTime.Now),
-                    AddressId = addressId
-                };
-
-                UserService.CreateUser(newUser);
-
-                //this be done by domain instead here
-                await _userRepository.SaveToDBAsync(newUser);
-
+                var userEntity = UserFormModel.ToUserEntity();
+                await UserService.AddUser(userEntity);
 
                 TempData["SuccessMessage"] = "User registered successfully!";
                 return RedirectToPage("Login");
