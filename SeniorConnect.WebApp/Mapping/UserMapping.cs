ea4d888c7fa29aba54304a29ec140610cce1d19f
yoomster <nperenboom@hotmail.com>
2024-12-25 15:13:28 +0100

    Added extension method for mapping form model to domains model

---
+++ SeniorConnect.WebApp/Mapping/UserMapping.cs
@@ -0,0 +1,25 @@
+﻿using CoreDomain;
+using CoreDomain.Users;
+using SeniorConnect.WebApp.Models;
+
+namespace SeniorConnect.WebApp.Mapping;
+
+public static class UserMapping
+{
+    public static User ToUserEntity(this UserFormModel formModel)
+    {
+        var newUser = new User
+        {
+            Email = formModel.Email,
+            FirstName = formModel.FirstName,
+            LastName = formModel.LastName,
+            Password = formModel.Password,
+            DateOfBirth = formModel.DateOfBirth.Date,
+            City = formModel.City,
+            Country = formModel.Country,
+            HouseNumber = formModel.HouseNumber,
+            StreetName = formModel.StreetName,
+            Zipcode = formModel.Zipcode,
+        };
+    }
+}
