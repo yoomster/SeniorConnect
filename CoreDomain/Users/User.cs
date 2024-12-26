fbb2367704069aeaf7d94fa630512474d1320e0f
yoomster <nperenboom@hotmail.com>
2024-12-26 20:50:38 +0100

    made properties required

--- CoreDomain/Users/User.cs
+++ CoreDomain/Users/User.cs
@@ -12,11 +12,11 @@ public class User
     public int Id;
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
-    public required string Email { get; set; } // verification for uniqueness
-    public required string Password { get; set; } // need to hash before sending to DB?
+    public required string Email { get; set; } 
+    public required string Password { get; set; } 
     public DateOnly DateOfBirth { get; set; }
-    public char? Gender { get; set; } 
-    public string? Origin { get; set; }
+    public required char Gender { get; set; } 
+    public required string Origin { get; set; }
     public DateOnly DateOfRegistration { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     public required string StreetName { get; set; }
     public required string HouseNumber { get; set; }
