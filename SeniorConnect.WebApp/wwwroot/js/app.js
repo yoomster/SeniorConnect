7da5ba2ba07ff211b5e7fd22f9f2095d60f9c78c
yoomster <nperenboom@hotmail.com>
2025-01-10 16:55:28 +0100

    added js and css from designer

---
+++ SeniorConnect.WebApp/wwwroot/js/app.js
@@ -0,0 +1,33 @@
+﻿
+// functie voor slideshow
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
+
+
+}
+
+
+// hieronder voor hamburger menu op mobiele versie
+
+const mobileMenu = document.getElementById("mobile-menu");
+const navList = document.querySelector("nav");
+
+mobileMenu.addEventListener("click", () => {
+    navList.classList.toggle("active");
+});
+
+
+
+
