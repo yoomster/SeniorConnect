d8d2937ccc721ccc42c3437ddce5342277b6b1b3
NaomiP <nperenboom@hotmail.com>
2025-01-11 15:42:46 +0100

    latest

--- SeniosConnect.Tests/UserTests.cs
+++ SeniosConnect.Tests/UserTests.cs
@@ -1,37 +1,38 @@
 ﻿using SeniorConnect.Domain;
-using SeniorConnect.Application.Services;
 
 
 namespace SeniorConnect.Tests;
 
 public class UserTests
 {
-    public void TestAdd()
-    {
-        var user = new User(
-        firstName: "Adam",
-        lastName: "Akil",
-        email: "adam.akil@example.com",
-        password: "Password123",
-        dateOfBirth: new DateOnly(1955, 8, 20),
-        gender: 'M',
-        origin: "Social Media",
-        maritalStatus: MaritalEnum.Married,
-        streetName: "Main Street",
-        houseNumber: "104",
-        zipcode: "5616TZ",
-        city: "Eindhoven",
-        country: "Netherlands"
-    );
+    User validUser = new(
+    firstName: "Adam",
+    lastName: "Akil",
+    email: "adam.akil@example.com",
+    password: "Password123",
+    dateOfBirth: new DateOnly(1955, 8, 20),
+    gender: 'M',
+    origin: "Social Media",
+    maritalStatus: MaritalEnum.Married,
+    streetName: "Main Street",
+    houseNumber: "104",
+    zipcode: "5616TZ",
+    city: "Eindhoven",
+    country: "Netherlands");
 
-        var user.CreateAccount();
-    }
 
+    //If the User class has any validation logic, you may need to mock any dependencies or
+    //add additional assertions to handle specific validation cases.
+    
     //Valid Input:ctor inits object correctly when all inputs are valid.
     [Fact]
     public void User_Creation_WithValidInput_ShouldSucceed()
     {
+        //Arrange
+        //Act
 
+
+        //Assert
     }
 
     //Invalid Email Test: Test with invalid email formats to verify validation 
