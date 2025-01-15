e183801b562897a19eaed2f7a99a4da0c6baa74d
yoomster <nperenboom@hotmail.com>
2025-01-15 21:37:22 +0100

    Added UserService for Single Respons.

---
+++ SeniorConnect.Application/Services/UserService.cs
@@ -0,0 +1,54 @@
+﻿using SeniorConnect.Application.Interfaces;
+using SeniorConnect.Domain;
+
+namespace SeniorConnect.Application.Services;
+public class UserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    public async Task CreateAccount(User user)
+    {
+        var hashedPassword = PasswordHashing.HashPassword(user.Password);
+
+        User newUser = new(
+                firstName: user.FirstName,
+                lastName: user.LastName,
+                email: user.Email,
+                password: hashedPassword,
+                dateOfBirth: user.DateOfBirth,
+                gender: user.Gender,
+                origin: user.Origin,
+                maritalStatus: user.MaritalStatus,
+                streetName: user.StreetName,
+                houseNumber: user.HouseNumber,
+                zipcode: user.Zipcode,
+                city: user.City,
+        country: user.Country
+            );
+
+        await _userRepository.CreateUserAsync(newUser);
+    }
+
+    public async Task<List<User>> GetAllAsync()
+    {
+        try
+        {
+            var users = await _userRepository.GetAllAsync();
+            return users;
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+
+    public void DeleteAccount(int userId)
+    {
+        _userRepository.DeleteAccountAsync(userId);
+    }
+}
