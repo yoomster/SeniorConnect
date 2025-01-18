f9550061596f30be2835f8557fbfe36c93f43f9d
yoomster <nperenboom@hotmail.com>
2025-01-18 23:06:45 +0100

    UserValidator now conform Open-Close Principle

--- SeniorConnect.Application/Services/UserValidator.cs
+++ SeniorConnect.Application/Services/UserValidator.cs
@@ -8,20 +8,33 @@ using System.Threading.Tasks;
 
 namespace SeniorConnect.Application.Services
 {
-    public class UserValidator : IValidator
+    public class AgeValidator : IValidator
     {
-        private readonly IUserRepository _userRepo;
-
-        public UserValidator(IUserRepository userRepo)
-        {
-            _userRepo = userRepo;
-        }
-
         public async Task<ValidationResult> IsValid(User user)
         {
             var result = new ValidationResult { IsSuccess = true };
 
             var minimumDateOfBirth = DateTime.Now.AddYears(-60).Date;
+            if (user.DateOfBirth > DateOnly.FromDateTime(minimumDateOfBirth))
+            {
+                result.IsSuccess = false;
+                result.Messages.Add("U moet minimaal 60 jaar oud zijn om u in te schrijven.");
+            }
+            return result;
+        }
+    }
+
+    public class EmailValidator : IValidator
+    {
+        private readonly IUserRepository _userRepo;
+        public EmailValidator(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;            
+        }
+
+        public async Task<ValidationResult> IsValid(User user)
+        {
+            var result = new ValidationResult { IsSuccess = true };
 
             if (await _userRepo.IsEmailDuplicate(user.Email))
             {
@@ -29,10 +42,21 @@ namespace SeniorConnect.Application.Services
                 result.Messages.Add("Het opgegeven e-mailadres wordt al gebruikt.");
             }
 
-            if (user.DateOfBirth > DateOnly.FromDateTime(minimumDateOfBirth))
+            return result;
+
+        }
+    }
+
+    public class PasswordStrengthValidator : IValidator
+    {
+        public async Task<ValidationResult> IsValid(User user)
+        {
+            var result = new ValidationResult { IsSuccess = true };
+
+            if (user.Password.Length < 6)
             {
                 result.IsSuccess = false;
-                result.Messages.Add("U moet minimaal 60 jaar oud zijn om u in te schrijven.");
+                result.Messages.Add("Wachtwoord moet min. 6 char lang zijn.");
             }
 
             return result;
