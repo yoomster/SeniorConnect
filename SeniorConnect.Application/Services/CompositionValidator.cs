65bc13cf09559d9519edd8303b5d935e449d78f7
yoomster <nperenboom@hotmail.com>
2025-01-19 13:01:51 +0100

    Composition to iterate through all the validators and returning results

---
+++ SeniorConnect.Application/Services/CompositionValidator.cs
@@ -0,0 +1,33 @@
+﻿using SeniorConnect.Application.Interfaces;
+using SeniorConnect.Domain;
+
+namespace SeniorConnect.Application.Services
+{
+    public class CompositionValidator : IValidator
+    {
+        private readonly IEnumerable<IValidator> _validators;
+
+        public CompositionValidator(IEnumerable<IValidator> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<ValidationResult> IsValid(User user)
+        {
+            var validationResult = new ValidationResult { IsSuccess = true };
+
+            foreach (var validator in _validators)
+            {
+                var result = await validator.IsValid(user);
+
+                if (!result.IsSuccess)
+                {
+                    validationResult.IsSuccess = false;
+                    validationResult.Messages.AddRange(result.Messages);
+                }
+            }
+
+            return validationResult;
+        }
+    }
+}
